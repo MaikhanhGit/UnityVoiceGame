ff3ae5b280fdb29bdae49898f1c24aa6b7654523
Maikhanh <maikhanh.dsn@gmail.com>
2025-09-15 12:01:37 -0500

    Start Main Menu
    Added Main menu and Exit menu

--- Assets/_Game/Scripts/GameManager.cs
+++ Assets/_Game/Scripts/GameManager.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] int _playSceneToLoad = 1;
+
     private void Update()
     {
         if (Keyboard.current.escapeKey.wasPressedThisFrame == true)
@@ -12,5 +15,18 @@ public class GameManager : MonoBehaviour
             Debug.Log("Exit Game");
             return;
         }
+    }
+
+    private void StartGame()
+    {
+        Debug.Log("Start Game");
+        SceneManager.LoadScene(_playSceneToLoad);
+    }
+
+    private void ExitGame()
+    {
+        Debug.Log("Exit");
+        
+
     }
 }
